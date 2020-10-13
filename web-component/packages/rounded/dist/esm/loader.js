d1e3cc3aba96184e63e351b55f2910460d58f161
Goran <gospaso@gmail.com>
2020-10-13 02:25:20 -0400

    fix(ronuded): update build files

---
+++ web-component/packages/rounded/dist/esm/loader.js
@@ -0,0 +1,17 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-e7d0555a.js';
+
+/*
+ Stencil Client Patch Esm v2.0.3 | MIT Licensed | https://stenciljs.com
+ */
+const patchEsm = () => {
+    return promiseResolve();
+};
+
+const defineCustomElements = (win, options) => {
+  if (typeof window === 'undefined') return Promise.resolve();
+  return patchEsm().then(() => {
+  return bootstrapLazy([["rounded-icon",[[1,"rounded-icon",{"ariaLabel":[1537,"aria-label"],"flip":[1],"name":[1],"src":[1],"icon":[8],"size":[8],"rotate":[1],"variant":[1],"strokeWidth":[8,"stroke-width"],"svgContent":[32],"components":[32]}]]]], options);
+  });
+};
+
+export { defineCustomElements };
