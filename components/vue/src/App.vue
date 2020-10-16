19bd657f2b513601a75112011283a1051f72e63d
Goran <gospaso@gmail.com>
2020-10-16 02:23:54 -0400

    feat(vue): init component scaffold

---
+++ components/vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <glyph-icon name="heart" style="font-size: 3rem" rotate="45" :transition="true" offset="0" />
+  </div>
+</template>
+
+<script>
+import GlyphIcon from '../GlyphIcon.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GlyphIcon
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
