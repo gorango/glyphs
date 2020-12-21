7088c1f43de6d70d851f67136cf6cbc4aa992e6d
Goran <gospaso@gmail.com>
2020-12-21 17:15:17 -0500

    feat(cli): add preview command
    Enable previewing synced icons using the data files from the "glyphs sync" output. Generate an HTML file containing the icons, sorted into categories, and displayed on an interval loop alternating between available variants.
    
    This helps to debug new icon sets before publishing.

---
+++ cli/lib/preview/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const pug = require('pug')
+
+module.exports = async function preview (dataDir) {
+  const metaFile = `${dataDir}/meta.json`
+  const componentsFile = `${dataDir}/components.json`
+  const filesExist = [
+    fs.existsSync(metaFile),
+    fs.existsSync(componentsFile)
+  ].reduce((bool, exists) => bool && exists, true)
+
+  if (!filesExist) {
+    console.log()
+    console.log('  The files required to generate the preview have not been found')
+    console.log('  Run "glyphs sync" to generate the required files')
+    console.log()
+    process.exit(1)
+  }
+
+  const meta = require(metaFile)
+  const components = require(componentsFile)
+
+  const { categories, variants } = meta
+  const first = Object.values(categories)[0][0]
+  const input = path.join(__dirname, 'template.pug')
+  const output = path.join(process.cwd(), 'preview.html')
+  const file = pug.compileFile(input, { pretty: true })({ first, categories, variants, components })
+
+  fs.writeFileSync(output, file)
+}
+
