6af7951ad43d82635c0a039ea028ac5981900824
Goran <gospaso@gmail.com>
2021-01-07 00:39:31 -0500

    feat(docs): add global content components

---
+++ docs/components/global/content/Picture.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div
+    img(:src='src', :alt='name')
+</template>
+
+<script>
+export default {
+  props: {
+    toggle: {
+      type: Boolean,
+      default: false
+    },
+    name: {
+      type: String,
+      default: null
+    }
+  },
+
+  data: () => ({
+    src: null
+  }),
+
+  mounted () {
+    const theme = 'dark'
+    const name = this.toggle
+      ? `${this.name
+        .split('.')
+        .reduce((str, seg, i) =>
+          !i ? seg : `${str}-${theme}.${seg}`, '')}`
+      : this.name
+    // this.src = require(`@/assets/content/${name}`)
+  }
+}
+</script>
