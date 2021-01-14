cd3e4cf848c3dc039fd12d49745db2d287291df0
Goran <gospaso@gmail.com>
2021-01-14 14:56:17 -0500

    fix(cli:gen): cleanup, update templates

--- cli/lib/generate/wc.js
+++ cli/lib/generate/wc.js
@@ -2,47 +2,49 @@ const { createVariants, s, createIndex } = require('./utils/template-utils')
 
 module.exports.index = createIndex
 
-module.exports.component = async ({ variants, className, tagName, set }) => `
+module.exports.component = async ({ set, variants, defaultVariant, componentName, className, tagName, ratios }) => `
 import { html, property, svg, define } from 'hybrids'
 import { transform } from '../utils'
 
 const ${className} = {
   size: '40',
-  color: 'currentColor',
-  variant: '',
-  colors: ['#000000', '#C4C4C4'],
+  variant: ${defaultVariant},
   strokeWidth: '3',
   strokeLinecap: 'round',
   strokeLinejoin: 'round',
   rotate: '0',
   flip: '',
-  render: ({ size, color, variant, strokeWidth, strokeLinecap, strokeLinejoin, rotate, flip }) => html\`
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="\${size}"
-      height="\${size}"
-      viewBox="0 0 80 80"
-      transform="\${transform(rotate, flip)}"
-    >
-      <slot></slot>
-      <g>\
-        ${await createVariants(variants, {
-          set,
-          parent: {
-            prepend: variant => `\n${s(8)}\${variant === "${variant}" && svg\``,
-            append: variant => `\n${s(8)}\`}`
-          },
-          child: {
-            prepend: child => `\n${s(10)}<${child.name}`,
-            append: child => `\n${s(10)}/>`,
-            space: '\n' + s(12),
-            attr: (attr, str) => `${attr}="\${${str}}"`,
-            default: (attr, val) => `${attr}="${val}"`
-          }
-        })}
-      </g>
-    </svg>
-  \`
+  render: ({ size, variant, strokeWidth, strokeLinecap, strokeLinejoin, rotate, flip }) => {
+    const ratios = ${JSON.stringify(ratios, null, 1).replace(/\n\s+/g, ' ')}
+    return ratios?.variants?.[variant] || ratios?.icons?.${componentName} || 1
+    return html\`
+      <svg
+        width="\${size * ratio}"
+        height="\${size}"
+        viewBox="0 0 80 80"
+        transform="\${transform(rotate, flip)}"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+      >
+        <slot></slot>
+        <g>\
+          ${await createVariants(variants, {
+            set,
+            parent: {
+              prepend: variant => `\n${s(8)}\${variant === "${variant}" && svg\``,
+              append: variant => `\n${s(8)}\`}`
+            },
+            child: {
+              prepend: child => `\n${s(10)}<${child.name}`,
+              append: child => `\n${s(10)}/>`,
+              space: '\n' + s(12),
+              attr: (attr, str) => `${attr}="\${${str}}"`,
+              default: (attr, val) => `${attr}="${val}"`
+            }
+          })}
+        </g>
+      </svg>
+    \`
 }
 
 define('${tagName}', ${className})
